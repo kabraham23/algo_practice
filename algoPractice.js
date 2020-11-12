5efe469481f9ab292ccb65ee3f178850fc4e2715
Katie <katie@katies-air.lan>
2020-11-12 11:23:05 -0500

    reverse words

--- algoPractice.js
+++ algoPractice.js
@@ -1,4 +1,6 @@
-// Day One - MERGING RANGES
+// Day One  
+
+// INTERVIEW CAKE - MERGING RANGES
 
 // let HiCal = (startTime, endTime) => {
 //     if (startTime[i] <= startTime[j])
@@ -32,3 +34,58 @@ console.log(mergeRanges(  [
     { startTime: 10, endTime: 12 },
     { startTime: 9,  endTime: 10 },
   ]));
+
+// LEET CODE - ADD TWO NUMBERS
+
+
+
+/////////////////////////////////////////////
+
+// Write a function that takes an array of characters and reverses the letters in place
+
+// function reverse(arrayOfChars) {
+//     let leftIndex = 0;
+//     let rightIndex = arrayOfChars.length-1;
+
+//     while (leftIndex < rightIndex) {
+//         const temp = arrayOfChars[leftIndex];
+//         arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+//         arrayOfChars[rightIndex] = temp;
+
+//         leftIndex++;
+//         rightIndex--;
+//     }
+    
+// };
+
+
+function reverseWords(message) {
+
+    // Decode the message by reversing the words
+    
+    reverseCharacters(message, 0, messsage.length-1);
+    
+    let currentWordStartIndex = 0;
+    for (let i = 0; i<=message.length; i++){
+      if (i === message.length || message[i] === ' '){
+        reverseCharacters(message, currentWordStartIndex, i-1);
+        currentWordStartIndex = i-1;
+      }
+    };
+    
+    function reverseCharacters(message, leftIndex, rightIndex) {
+      while (leftIndex < rightIndex){
+        const temp = message[leftIndex];
+        message[leftIndex] = message[rightIndex];
+        message[rightIndex] = temp;
+        leftIndex++;
+        rightIndex--;
+      }
+    };
+    
+    
+  
+  }
+  console.log(reverseWords("katie is cool"));
+  
+  
