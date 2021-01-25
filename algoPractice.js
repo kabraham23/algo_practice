991fc675fc1e38358c0c36227ea2228c758af70a
Katie Abraham <katie.t.abraham@gmail.com>
2021-01-25 15:51:38 -0500

    sliding window problems

--- algoPractice.js
+++ algoPractice.js
@@ -327,18 +327,61 @@
 // Count Unique Values
 ////////////////////////////////////////
 
-function countUniqueValues(arr) {
-    if (arr.length === 0) return 0;
-    let i = 0;
-    for (let j = 1; j < arr.length; j++) {
-        if (arr[i] !== arr[j]){
-            i++;
-            arr[i] = arr[j]
-        }
-        // console.log(i,j);
-    }
-    return i + 1;
-};
+// function countUniqueValues(arr) {
+//     if (arr.length === 0) return 0;
+//     let i = 0;
+//     for (let j = 1; j < arr.length; j++) {
+//         if (arr[i] !== arr[j]){
+//             i++;
+//             arr[i] = arr[j]
+//         }
+//         // console.log(i,j);
+//     }
+//     return i + 1;
+// };
 
-// countUniqueValues();
-console.log(countUniqueValues([1,1,2,3,4,5,5,6,7]));
+// // countUniqueValues();
+// console.log(countUniqueValues([1,1,2,3,4,5,5,6,7]));
+
+///////////////////////////////
+// Sliding Window Problems
+///////////////////////////////
+
+// Useful in keeping track of a subset in a larger array or string
+
+function maxSubarraySum(arr, num) {
+    if (num > arr.length){
+        return null;
+    }
+    let max = -Infinity;
+    for (let i = 0; i < arr.length - num +1; i++){
+        temp = 0;
+        for (let j = 0; j < num; j++){
+            temp += arr[i+j];
+        }
+        if (temp > max){
+            max = temp;
+        }
+    }
+    return max;
+}
+console.log(maxSubarraySum([2,5,6,9,2,1,8,5,6,3],3))
+// this method is very inefficient
+
+// SLIDING WINDOW
+
+function maxSubarray(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++){
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+console.log(maxSubarray([2,5,6,9,2,1,8,5,6,3],3))
