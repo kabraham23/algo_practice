7c46ba893ee05ad1bdc622ce82518c19452904df
Katie Abraham <katie.t.abraham@gmail.com>
2021-02-10 09:20:26 -0500

    memoize nth fib solution

--- algoPractice.js
+++ algoPractice.js
@@ -560,13 +560,26 @@
 // Nth Fibonacci
 ///////////////////////////////////
 
-function getNthFib(n) {
-    if (n === 2) {
-        return 1;
-    } else if (n === 1) {
-        return 0;
-    } else {
-        return getNthFib(n - 1) + getNthFib(n - 2);
-    }
-}
-console.log(getNthFib(6));
+// bad time complexity
+// function getNthFib(n) {
+//     if (n === 2) {
+//         return 1;
+//     } else if (n === 1) {
+//         return 0;
+//     } else {
+//         return getNthFib(n - 1) + getNthFib(n - 2);
+//     }
+// }
+// console.log(getNthFib(6));
+
+// memoize hash table O(n) time and space complexity
+// function getNthFib(n, memoize = {1: 0, 2: 1}) {
+//     if (n in memoize) {
+//         return memoize[n];
+//     } else {
+//         memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n - 2, memoize);
+//         return memoize[n];
+//     }
+// }
+// console.log(getNthFib(6));
+
