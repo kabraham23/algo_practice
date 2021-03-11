93a5ead71c4ad249404395628609337329863503
Katie Abraham <katie.t.abraham@gmail.com>
2021-03-11 11:09:57 -0500

    monotonic array

--- algoPractice.js
+++ algoPractice.js
@@ -819,19 +819,59 @@
 // Move Element to End
 ///////////////////////////////
 
-function moveElementToEnd(array, toMove) {
-	let i = 0;
-	let j = array.length -1;
-	while (i < j) {
-		while (i < j && array[j] === toMove) j--;
-		if (array[i] === toMove) swap(i, j, array);
-		i++;
+// function moveElementToEnd(array, toMove) {
+// 	let i = 0;
+// 	let j = array.length -1;
+// 	while (i < j) {
+// 		while (i < j && array[j] === toMove) j--;
+// 		if (array[i] === toMove) swap(i, j, array);
+// 		i++;
+// 	}
+// 	return array;
+// }
+
+// function swap(i, j, array) {
+// 	const temp = array[j];
+// 	array[j] = array[i];
+// 	array[i] = temp;
+// }
+
+////////////////////////////////
+// Monotonic Array
+////////////////////////////////
+
+// MY ANSWER
+
+function isMonotonic(array) {
+	monotonicArray = false;
+	array.sort((a, b) => a - b);
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === array[i + 1])
+			momontonicArray = true;
 	}
-	return array;
+	return monotonicArray;
 }
 
-function swap(i, j, array) {
-	const temp = array[j];
-	array[j] = array[i];
-	array[i] = temp;
+// REAL ANSWER
+
+function isMonotonic(array) {
+	if (array.length <= 2) return true;
+	
+	let direction = array[1] - array[0];
+	for (let i = 2; i < array.length; i++) {
+		if (direction === 0) {
+			direction = array[i] - array[i - 1];
+			continue;
+		}
+		if (breaksDirection(direction, array[i - 1], array[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function breaksDirection(direction, previousInt, currentInt) {
+	const difference = currentInt - previousInt;
+	if (direction > 0) return difference < 0;
+	return difference > 0;
 }
