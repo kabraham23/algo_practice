133def46b539b09195f154628ecb1f82bcdcdba8
Katie Abraham <katie.t.abraham@gmail.com>
2021-03-31 10:47:00 -0400

    remove dupes from sorted array

--- algoPractice.js
+++ algoPractice.js
@@ -963,3 +963,19 @@ function arrayOfProducts(array) {
 	}
 	return products;
 }
+
+////////////////////////////////
+// Remove Duplicates from Sorted Array
+////////////////////////////////
+
+var removeDuplicates = function(nums) {
+  if (nums.length === 0) return 0;
+  i = 0;
+  for (let j = 1; j < nums.length; j++) {
+      if (nums[j] !== nums[i]) {
+          i++;
+          nums[i] = nums[j];
+      }
+  }
+  return i + 1;
+};
