ca6bf52a66fa0c1670e8973afa0621871396429c
Katie Abraham <katie.t.abraham@gmail.com>
2021-04-22 09:45:04 -0400

    Palindrome Check

--- algoPractice.js
+++ algoPractice.js
@@ -1115,69 +1115,110 @@
 // Linked List Practice (Colt Steele)
 ///////////////////////////////
 
+// class Node{
+//   constructor(val){
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// class SinglyLinkedList{
+//   constructor(){
+//     this.head = null;
+//     this.tail = null;
+//     this.length = 0;
+//   }
+//   push(val){
+//     let newNode = new Node(val);
+//     if (!this.head){
+//       this.head = newNode;
+//       this.tail = this.head;
+//     } else {
+//       this.tail.next = newNode;
+//       this.tail = newNode;
+//     } 
+//     this.length++;
+//     return this;
+//   }
+// }
+
+// var list = new SinglyLinkedList()
+// list.push("Hello")
+// list.push("Goodbye")
+
+// /////////////////////
+// // Linked List - Pop
+// /////////////////////
+
+// class Node{
+//   constructor(val){
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// class SinglyLinkedList{
+//   constructor(){
+//     this.head = null;
+//     this.tail = null;
+//     this.length = 0;
+//   }
+//   pop(val){
+//     if(!this.head) return undefined;
+//     let current = this.head;
+//     let newTail = current;
+//     while(current.next){
+//       newTail = current;
+//       current = current.next;
+//     }
+//     this.tail = newTail;
+//     this.tail.next = null;
+//     this.length--;
+//     if(this.length === 0){
+//       this.head = null;
+//       this.tail = null;
+//     }
+//     return current;
+//   }
+// }
+
+///////////////////////////
+// Linked List (shift)
+///////////////////////////
+
 class Node{
-  constructor(val){
-    this.val = val;
-    this.next = null;
-  }
-}
-
-class SinglyLinkedList{
-  constructor(){
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-  push(val){
-    let newNode = new Node(val);
-    if (!this.head){
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    } 
-    this.length++;
-    return this;
-  }
-}
-
-var list = new SinglyLinkedList()
-list.push("Hello")
-list.push("Goodbye")
-
-/////////////////////
-// Linked List - Pop
-/////////////////////
-
-class Node{
-  constructor(val){
-    this.val = val;
-    this.next = null;
-  }
-}
-
-class SinglyLinkedList{
-  constructor(){
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-  pop(val){
-    if(!this.head) return undefined;
-    let current = this.head;
-    let newTail = current;
-    while(current.next){
-      newTail = current;
-      current = current.next;
+    constructor(val){
+      this.val = val;
+      this.next = null;
     }
-    this.tail = newTail;
-    this.tail.next = null;
-    this.length--;
-    if(this.length === 0){
+  }
+  
+  class SinglyLinkedList{
+    constructor(){
       this.head = null;
       this.tail = null;
+      this.length = 0;
     }
-    return current;
+  shift(){
+    if(!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if(this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
   }
+}
+
+////////////////////////
+// is Palindrome
+////////////////////////
+
+function isPalindrome(string) {
+	const reversedString = [];
+	for (let i = string.length - 1; i >= 0; i--) {
+		reversedString.push(string[i]);
+	}
+	return string === reversedString.join('');
 }
