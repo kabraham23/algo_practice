0137f6104d98adb3d3d5312b6a7d17820d024449
Katie Abraham <katie.t.abraham@gmail.com>
2021-04-23 13:48:42 -0400

    linked list unshift

--- algoPractice.js
+++ algoPractice.js
@@ -1211,6 +1211,36 @@
 //   }
 // }
 
+/////////////////////////////////
+// Linked Lists (Unshift)
+/////////////////////////////////
+
+class Node{
+      constructor(val){
+        this.val = val;
+        this.next = null;
+      }
+    }
+    
+    class SinglyLinkedList{
+      constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+      }
+      unshift(val){
+        let newNode = new Node(val);
+        if(!this.head) {
+          this.head = newNode;
+          this.tail = this.head;
+        } else {
+        newNode.next = this.head;
+        this.head = newNode;
+        }
+        this.length++;
+        return this;
+      }
+    }
 ////////////////////////
 // is Palindrome
 ////////////////////////
