fa107381e93b30e1f83e6c2bebd776d344fb9c9b
Katie Abraham <katie.t.abraham@gmail.com>
2021-04-26 15:41:24 -0400

    linked list set

--- algoPractice.js
+++ algoPractice.js
@@ -1270,6 +1270,43 @@ class SinglyLinkedList{
     return current;
   }
 }
+
+///////////////////////////
+// Linked List - Set
+///////////////////////////
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList{
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+  set(index, value){
+    let foundNode = this.get(index);
+    if (foundNode){
+      foundNode.value = value;
+      return true;
+    }
+    return false
+  }
+}
+
 ////////////////////////
 // is Palindrome
 ////////////////////////
