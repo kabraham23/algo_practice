ecbcf9905a6d5a236039930f2602acb844493c6d
Katie Abraham <katie.t.abraham@gmail.com>
2021-04-27 10:42:26 -0400

    doubly linked list push and pop

--- algoPractice.js
+++ algoPractice.js
@@ -1297,7 +1297,7 @@ class SinglyLinkedList{
     }
     return current;
   }
-  set(index, value){
+  set(index, value){ /*this needs the get function to work!*/
     let foundNode = this.get(index);
     if (foundNode){
       foundNode.value = value;
@@ -1306,6 +1306,53 @@ class SinglyLinkedList{
     return false
   }
 }
+
+/////////////////////////////
+// Doubly Linked Lists
+/////////////////////////////
+
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val){
+    let newNode = new Node(val);
+    if (this.length ===0){
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop(){
+    if(!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+}
+
 
 ////////////////////////
 // is Palindrome
