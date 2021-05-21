12c047c3f72d70e43ff4cbf271e5f8155d7b0ae8
Katie A <katie.t.abraham@gmail.com>
2021-05-21 10:18:25 -0400

    run-length encoding

--- algoPractice.js
+++ algoPractice.js
@@ -1468,3 +1468,29 @@ function validateBSTHelper(tree, minValue, maxValue) {
 	const leftIsValid = validateBSTHelper(tree.left, minValue, tree.value);
 	return leftIsValid && validateBSTHelper(tree.right, tree.value, maxValue);
 }
+
+///////////////////////////////////////
+// Run-Length Encoding
+///////////////////////////////////////
+
+function runLengthEncoding(string) {
+	const encodedStringCharacters = [];
+	let currentRunLength = 1;
+	
+	for (let i = 1; i < string.length; i++) {
+		const currentChar = string[i];
+		const previousChar = string[i - 1];
+		
+		if (currentChar !== previousChar || currentRunLength === 9){
+			encodedStringCharacters.push(currentRunLength.toString());
+			encodedStringCharacters.push(previousChar);
+			currentRunLength = 0;
+		}
+		currentRunLength++;
+	}
+	
+	encodedStringCharacters.push(currentRunLength.toString());
+	encodedStringCharacters.push(string[string.length - 1]);
+	
+	return encodedStringCharacters.join('');
+}
