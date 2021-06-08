dc4fb21cc186e84231531ba1e5363d9759b5d1b7
Katie A <katie.t.abraham@gmail.com>
2021-06-08 10:41:47 -0400

    first non repeating character

--- algoPractice.js
+++ algoPractice.js
@@ -1512,3 +1512,39 @@ function generateDocument(characters, document) {
 	}
   return true;
 }
+
+//////////////////////////
+// First non-repeating character
+//////////////////////////
+
+// brute force
+
+function firstNonRepeatingCharacter(string) {
+  for (let i = 0; i < string.length; i++) {
+		let foundDuplicate = false;
+		for (let j = 0; j < string.length; j++) {
+			if(string[i] === string[j] && i !== j) foundDuplicate = true;
+		}
+		if (!foundDuplicate) return i;
+	}
+	return -1;
+}
+
+
+// Hash tables
+
+function firstNonRepeatingCharacter(string) {
+	const charFrequencies = {};
+	
+	for (const char of string) {
+		if (!(char in charFrequencies)) charFrequencies[char] = 0;
+		charFrequencies[char]++;
+	}
+	
+	for (let i = 0; i < string.length; i++) {
+		const char = string[i];
+		if (charFrequencies[char] === 1) return i;
+	}
+	
+  return -1;
+}
