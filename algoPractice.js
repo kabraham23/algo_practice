0db5bc67f6dcb0677b59a3f340a97f3494d52148
Katie A <katie.t.abraham@gmail.com>
2021-08-13 15:51:07 -0400

    insertion sort

--- algoPractice.js
+++ algoPractice.js
@@ -1649,3 +1649,24 @@ function removeDuplicatesFromLinkedList(linkedList) {
 	}
   return linkedList;
 }
+
+////////////////////////////////////
+// Insertion Sort
+////////////////////////////////////
+
+function insertionSort(array) {
+	for (let i = 1; i < array.length; i++) {
+		let j=i;
+		while (j > 0 && array[j] < array[j - 1]) {
+			swap(j, j - 1, array);
+			j-= 1;
+		}
+	}
+	return array;
+}
+
+function swap(i, j, array) {
+	const temp = array[j];
+	array[j] = array[i];
+	array[i] = temp;
+}
