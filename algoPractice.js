c2cd1045f6b7808ceb287a1756c07b218a63c9dc
Katie A <katie.t.abraham@gmail.com>
2021-08-19 11:30:17 -0400

    caesar cipher encryptor

--- algoPractice.js
+++ algoPractice.js
@@ -1670,3 +1670,22 @@ function swap(i, j, array) {
 	array[j] = array[i];
 	array[i] = temp;
 }
+
+/////////////////////////////////
+// Caesar Cipher Encryptor
+/////////////////////////////////
+
+function caesarCipherEncryptor(string, key) {
+	const newLetters = [];
+	const newKey = key % 26;
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	for (const letter of string) {
+		newLetters.push(getNewLetter(letter, newKey, alphabet));
+	}
+	return newLetters.join('')
+}
+
+function getNewLetter(letter, key, alphabet) {
+	const newLetterCode = alphabet.indexOf(letter) + key;
+	return alphabet[newLetterCode % 26];
+}
